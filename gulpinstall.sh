fe8b4912450411e9122e8ae45b295b19832886f5
Kerick Howlett <bigk5000@gmail.com>
2017-01-24 23:39:22 -0500

    Worked on SVGs-dash problem.

---
+++ gulpinstall.sh
@@ -0,0 +1,15 @@
+# This script is designed to install all the node modules with one command.
+#
+# @NOTE: If your computer runs on linux, uncomment the first command.
+#
+# @author: Kerick Howlett
+# @date: January 24, 2017
+
+# sudo apt-get install nodejs-legacy
+sudo npm install gulp
+sudo npm install gulp -g
+sudo npm install gulp-sass
+sudo npm install gulp-minify-css
+sudo npm install gulp-rename
+sudo npm install gulp-concat
+sudo npm install gulp-uglify
