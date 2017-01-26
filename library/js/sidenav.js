e564f4eb91e77520b0d9bf434a0516dc8d1cb027
Kerick Howlett <kerick@dbswebsite.com>
2017-01-26 17:12:15 -0500

    Worked on mobile navbar.

--- library/js/sidenav.js
+++ library/js/sidenav.js
@@ -7,8 +7,15 @@
 
 ( function( $ ) {
 	$( document ).ready( function() {
-		$( '.nav_links li' ).hover( function() {
-			$( '.nav_links li' ).not( this ).toggleClass( 'unselected_link' );
-		} );
+		if ( $( window ).width() > 767 ) { // Desktop
+			$( '.nav_links li' ).hover( function() {
+				$( '.nav_links li' ).not( this ).toggleClass( 'unselected_link' );
+			} );
+		} else {
+			$( '.menu-icon' ).click( function() { // Mobile
+				$( this ).toggleClass( 'clicked-icon' );
+				$( '.nav_links' ).toggleClass( 'show_links' );
+			} );
+		}
 	} );
 } )( jQuery );
