bbc9067b417c756cf8667f4a4e0550c851a2a2e5
Kerick Howlett <kerick@dbswebsite.com>
2017-02-24 17:09:42 -0500

    Began validation.

--- library/js/connect.js
+++ library/js/connect.js
@@ -1,6 +1,6 @@
 /**
  * This JavaScript/jQuery file is to operate the validation for the contact/connect
- * form.
+ * form, as well as the effect for the label.
  *
  * @author Kerick Howlett
  **/
@@ -10,9 +10,64 @@
 			$( this ).parent().find( 'label' ).addClass( 'selected_input' );
 		} );
 		$( '.form__block input, .form__block textarea' ).focusout( function() {
-			console.log($( this ).val());
 			if ( !$( this ).val() ) {
 				$( this ).parent().find( 'label' ).removeClass( 'selected_input' );
+			}
+		} );
+		$( '#submit' ).click( function() {
+			if ( !$( '#email' ).val() ) {
+
+				/**
+				 * Returns status message after submitting form.
+				 *
+				 * @return {[type]} [description]
+				 **/
+				function returnMessage( status, fields ) {
+
+
+
+				};
+
+				/**
+				 * Checks to see if the email address is valid.
+				 * @param  {string}  address Entered email address.
+				 * @return {Boolean}         Is the email address valid?
+				 **/
+				function isEmailValid( address ) {
+					var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+					return pattern.test( address );
+				};
+
+				var name = $( '#name' ).val();
+				var email = $( '#your_email' ).val();
+				var subject = $( '#subject' ).val();
+				var message = $( '#message' ).val();
+
+				if ( name && email && message ) {
+
+					// TODO: Create post function to email PHP file.
+
+				} else {
+					var fields = [];
+					if ( !name ) {
+						fields[ fields.length ] = 'name';
+					}
+					if ( !email ) {
+						fields[ fields.length ] = 'email';
+					} else {
+						if ( isEmailValid( email ) ) {
+							fields[ fields.length ] = 'valid_email';
+						}
+					}
+					if ( !message ) {
+						fields[ fields.length ] = 'message';
+					}
+				}
+
+				returnMessage( 'fail', fields );
+
+			} else {
+				console.info( 'NO SPAM ALLOWED!' );
 			}
 		} );
 	} );
--- mail.php
+++ mail.php
@@ -1,10 +1,11 @@
 <?php
-/**
- * This is the back-end functionality for the connect/contact form of the site.
- *
- * @author Kerick Howlett
- **/
 
+	/**
+	 * This is the back-end functionality for the connect/contact form of the site.
+	 *
+	 * @author Kerick Howlett
+	 **/
 
+	
 
 ?>
